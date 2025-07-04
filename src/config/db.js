e7d5f194d69c074da11cb75a6b8d4f0530b4ae9e
manuel <manuelgonzga@uma.es>
2025-07-04 13:21:57 -0300

    Upload Date

--- src/config/db.js
+++ src/config/db.js
@@ -14,7 +14,7 @@ export async function initDB() {
         title VARCHAR(255) NOT NULL,
         amount DECIMAL(10,2) NOT NULL,
         category VARCHAR(255) NOT NULL,
-        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
       );
     `;
 
